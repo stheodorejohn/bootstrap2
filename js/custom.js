ec64e4bd6fcf38e890ca477c4a4629f0981073fe
stheodorejohn <96522630+stheodorejohn@users.noreply.github.com>
2022-01-25 23:45:41 +0530

    final v 1

--- js/custom.js
+++ js/custom.js
@@ -19,14 +19,84 @@ jQuery(document).ready(function ($) {
     var hamburgerClose = $('.hamburger_close');
     var fsOverlay = $('.fs_menu_overlay');
 
-
-    initFavorite();
+     initFavorite();
     initIsotopeFiltering();
     initTimer();
     initSlider();
+ 
 
     // 2.  Inits Menu
 
+    // function initMenu() {
+    //     if (hamburger.length) {
+    //         hamburger.on('click', function () {
+    //             if (!menuActive) {
+    //                 openMenu();
+    //             }
+    //         });
+    //     }
+    //     if (fsOverlay.length) {
+    //         fsOverlay.on('click', function () {
+    //             if (menuActive) {
+    //                 closeMenu();
+    //             }
+
+
+    //         });
+
+    //     }
+
+    //     if (hamburgerClose.length) {
+    //         hamburgerClose.on('click', function () {
+    //             if (menuActive) {
+    //                 closeMenu();
+    //             }
+
+
+    //         });
+
+    //     }
+
+    //     if ($('.menu_item'), length) {
+    //         var items = document.getElementsByClassName('menu_item');
+    //         var i;
+
+    //         for (i = 0; i < items.length; i++) {
+    //             if (items[i].classList.contains("has-children")) {
+    //                 items[i].onclick = function () {
+    //                     this.classList.toggle("active");
+    //                     var panel = this.children[1];
+    //                     if (panel.style.maxHeight) {
+    //                         panel.style.maxHeight = null;
+
+    //                     }
+    //                     else {
+    //                         panel.style.maxHeight = panel.scrollHeight + "px";
+
+    //                     }
+    //                 }
+    //             }
+    //         }
+
+    //     }
+
+
+    // }
+
+
+    // function openMenu() {
+    //     menu.addClass('active');
+    //     fsOverlay.css('pointer-events', "auto");
+    //     menuActive = true;
+    // }
+
+
+
+    // function closeMenu() {
+    //     menu.removeClass('active');
+    //     fsOverlay.css('pointer-events', "none");
+    //     menuActive = false;
+    // }
 
 
 
@@ -142,49 +212,45 @@ jQuery(document).ready(function ($) {
 
     // 6.  Init Slider
 
-function initSlider()
-{
-    if($('.product_slider').length)
-    {
-        var slider1 = $('.product_slider');
+    function initSlider() {
+        if ($('.product_slider').length) {
+            var slider1 = $('.product_slider');
 
-        slider1.owlCarousel({
-            loop:false,
-            dots:false,
-            nav:false,
-            responsive:
-            {
-                0:{items:1},
-                480:{items:2},
-                768:{items:3},
-                991:{items:4},
-                1280:{items:5},
-                1440:{items:6},
+            slider1.owlCarousel({
+                loop: false,
+                dots: false,
+                nav: false,
+                responsive:
+                {
+                    0: { items: 1 },
+                    480: { items: 2 },
+                    768: { items: 3 },
+                    991: { items: 4 },
+                    1280: { items: 5 },
+                    1440: { items: 6 },
 
 
+                }
+            });
+
+            //01.53.16
+            if ($('.product_slider_nav_left').length) {
+                $('.product_slider_nav_left').on('click', function () {
+                    slider1.trigger('prev.owl.carousel');
+                });
             }
-        });
 
-        //01.53.16
-        if($('.product_slider_nav_left').length)
-        {
-            $('.product_slider_nav_left').on('click',function(){
-                slider1.trigger('prev.owl.carousel');
-            });
+
+            if ($('.product_slider_nav_right').length) {
+                $('.product_slider_nav_right').on('click', function () {
+                    slider1.trigger('next.owl.carousel');
+                });
+            }
+
+
+
         }
-
-
-        if($('.product_slider_nav_right').length)
-        {
-            $('.product_slider_nav_right').on('click',function(){
-                slider1.trigger('next.owl.carousel');
-            });
-        }
-
-
-
     }
-}
 
 
 
